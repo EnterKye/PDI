c1ce36ea196318fb400da860d46c20778b95d18b
cn005897 <1098120843@qq.com>
2021-01-04 16:52:12 +0800

    first

---
+++ layui/index.js
@@ -0,0 +1,10 @@
+﻿
+window.onload = function () {
+    if (sessionStorage.getItem("user") != null) {
+        window.parent.document.getElementById("uid").innerHTML = sessionStorage.getItem("user");
+        window.parent.document.getElementById("loginTo").innerHTML = "注销";
+    }
+    if (sessionStorage.getItem("user") == null) {
+        $(location).attr('href', 'login.html');
+    }
+}
